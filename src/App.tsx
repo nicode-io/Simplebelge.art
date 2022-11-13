c0eb6acf704ef0ca2f02399f9f382ba017328d06
Nicolas Denoël <nicolas.denoel@domintell.com>
2022-11-13 21:43:11 +0100

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simplebelge.art Nginx multiple test</h1>
+      </header>
+    </div>
+  );
+}
